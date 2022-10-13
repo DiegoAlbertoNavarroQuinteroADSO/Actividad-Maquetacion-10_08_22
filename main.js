369c9fa189b72c8556c60c531cde4d4c91695182
DiegoAlbertoNavarroQuinteroADSO <diegonavarro098@gmail.com>
2022-10-13 12:18:24 -0500

    co

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    Document.queryselector('.starbucks').src = anything;
+      }
+
+      function changeCircleColor(Color){
+          const circle = document.querySelector('.circle');
+      circle.style.background = color;
+    }
